03ec8a846cce01ca35430faba10b0655d840b5c2
Nick Kossifidis <mickflemm@gmail.com>
2018-12-28 19:50:54 +0200

    Initial commit

---
+++ files/udhcpc_handler.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ -z ${1} ]]; then
+	echo "No action provided"
+	return -1;
+fi
+
+RESOLV_CONF="/etc/resolv.conf"
+NTP_CONF="/etc/ntp.conf"
+
+case "${1}" in
+	defconfig)
+		ip addr flush dev ${interface}
+		;;
+	renew | bound)
+		ip addr add ${ip}/${mask} dev ${interface}
+		if [[ -n "${router}" ]]; then
+			ip route add default via ${router%% *} dev ${interface}
+		fi
+		if [[ -n ${domain} ]]; then
+			echo "search ${domain}" > ${RESOLV_CONF}
+		fi
+		for i in ${dns}; do
+			echo "nameserver ${i}" >> ${RESOLV_CONF}
+		done
+		for i in ${ntpsrv}; do
+			echo "server ${1}" >> ${NTP_CONF}
+		done
+		echo "server time.google.com" >> ${NTP_CONF}
+		;;
+esac
+
+return 0
+
